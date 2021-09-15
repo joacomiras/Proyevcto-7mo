aa78e78810be363839e7336fe355076033819e3f
Luciano Riente <lucianotomasriente@gmail.com>
2021-09-15 16:26:23 -0300

    Se modifica la condicion de calculo de angulos del sol y algunos comentarios

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <RTClib.h> /* Inclusión de la libreria para poder utilizar el módulo RTC */
 /* Creo que habría que borrar la librería TimerOne */
 
-RTC_DS3231 rtc; /* Se inicializa el objeto para el modelo del RTC (No hay para el modelo que tenemos nosotros , 1302 , probar con 1307) */
+RTC_DS3231 rtc; /* Se inicializa el objeto para el modelo del RTC */
 
 float lat = -34.6667; /* Latitud de Ituzaingó */
 float lon = -58.6667; /* Longitud de Ituzaingó */
@@ -61,14 +61,14 @@ void loop() {
   Serial.println(medir);
   // ######################## //
 
-  // Esto después hay que hacerlo con horas y no con minutos
-  if( minutos!=contadorDeMinutos ) {
+  // Si el divisor resistivo da una tensión menor a 15 volts , hace otra vez los cálculos del sol , o si nunca hizo los cálculos los hace (el inicio del programa)
+  if( medir < 1.5 || contadorDeMinutos == 0) {
     // ### CÁLCULOS DEL SOL ### //
     float decl = (23.45 * sin(PI / 180 * ((0.9863013699) * (dn - 81)))); /* Cálculo de la declinación del sol */
     float r = (((dn - 1) * 0.9863013699) * (PI / 180)); /* Variable para el calculo de edn */
     float edn = (13.752 * (0.075 + 1.868 * cos(r) - 32.077 * sin(r) - 14.615 * cos(2 * r) - 40.89 * sin(2 * r))); /* Es la ecuación del tiempo expresada en segundos, la  cual  determina  la  diferencia  entre  el  tiempo  solar medio y el tiempo solar aparente, y depende del día del año */
     float tsv = ((hora * 240 * (latm - lon) + edn) / 3600); /* Tiempo solar verdadero,  hora definida por la posición del Sol en lugar de la hora reloj */
-    float anghor = (360 * ((tsv - 12) / 24)); /* Es el`ángulo horario, representa  la  posicion  relativa  del  Sol  a la  vertical  del  observador  en  el  plano  de  su  trayectoria  para un día  dado */
+    float anghor = (360 * ((tsv - 12) / 24)); /* Es el ángulo horario, representa  la  posicion  relativa  del  Sol  a la  vertical  del  observador  en  el  plano  de  su  trayectoria  para un día  dado */
     float elv = (asin((cos(PI / 180 * lat) * cos(PI / 180 * decl) * cos(PI / 180 * anghor) + (sin(PI / 180 * lat) * sin(PI / 180 * decl))))); /* Cálculo de la elevación del sol */
     float azimut = (acos((sin(elv) * sin(PI / 180 * lat) - sin(PI / 180 * decl)) / (cos(elv) * cos(PI / 180 * lat)))); /* Cálculo del azimut del sol */
     if (anghor > 0) azimut = 360 - azimut; /* Si el ángulo horario es negativo , el valor del azimut es absoluto (no importa si es positivo o negativo). Si el ángulo horario es positivo , se debe hacer 360 - lo que da el azimut */
@@ -98,8 +98,6 @@ void loop() {
     // ### Movimiento de servos ### //
     // servoElevacion.attach(5);
     // servoAzimut.attach(10);
-    // servoElevacion.write(180);
-    // servoAzimut.write(180);
     // servoElevacion.write((180 / PI) * elv);
     // servoAzimut.write((180 / PI) * azimut);
     // ######################## //
